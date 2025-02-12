2ae43ea5a9470bd9fa997ea0352e096904909c47
raunakmishra18 <mraunak255@gmail.com>
2025-02-13 01:02:07 +0530

    day1

---
+++ datatype.java
@@ -0,0 +1,19 @@
+public class datatype {
+    public static void main(String[] args) {
+        int number=223;
+        int yournumber;
+        yournumber=number;
+        System.out.println(yournumber);
+        yournumber=220;
+        System.out.println(yournumber);
+        boolean is=true;
+        System.out.println(is);
+        float myfloat=0.3f;
+        System.out.println(myfloat);
+        double mydouble=0.43;
+        System.out.println(mydouble);
+        String wish="raunak";
+        System.out.println(wish);
+
+    }
+}
---
+++ oattern.java
@@ -0,0 +1,7 @@
+public class oattern {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****");
+        System.out.println("****\n***\n**\n*");
+        System.out.println("   *\n  **\n ***\n****");
+    }
+}
