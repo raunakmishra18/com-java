289d570d8ba530e977f43223b3b610284910366f
raunakmishra18 <mraunak255@gmail.com>
2025-02-17 11:17:35 +0530

    arrayfirstinput

---
+++ someoftwonumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class someoftwonumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a=sc.nextInt();
+        System.out.println("enter secound number");
+        int b =sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
---
+++ userinput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class userinput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the name ");
+        String input=sc.nextLine();
+        System.out.println("enter the age");
+        int age =sc.nextInt();
+    }
+}
